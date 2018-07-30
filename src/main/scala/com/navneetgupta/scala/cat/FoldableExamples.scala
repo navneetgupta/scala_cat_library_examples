dfc3d0a1bc7aa96cf0691db06100c95ec26b584f
Navneet Gupta <navneet_gupta@tutamail.com>
2018-07-30 16:30:45 +0400

    Changes

---
+++ src/main/scala/com/navneetgupta/scala/cat/FoldableExamples.scala
@@ -0,0 +1,12 @@
+package com.navneetgupta.scala.cat
+
+import cats._
+
+object FoldableExamples extends App {
+	import cats.implicits._
+
+	/**
+	 * For DS that can be folded over like  List/Set
+	 *
+	 */
+}
